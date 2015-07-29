2705bde38f720596268edcb56d5f766552f46667
douya <douya@douyadeMacBook-Air.local>
2015-07-29 11:53:13 +0800

    点击图片显示大图

---
+++ DYBigImageScan.h
@@ -0,0 +1,14 @@
+//
+//  DYBigImageScan.h
+//  豆芽学堂
+//
+//  Created by douya on 15/6/18.
+//  Copyright (c) 2015年 douya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface DYBigImageScan : NSObject
++(void)showImage:(UIImageView *)avatarImageView;
+@end
